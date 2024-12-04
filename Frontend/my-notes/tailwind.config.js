40dffd686c880eb13889bb88439becea95fe366d
Artur Odenbakh <arturodenbakh@gmail.com>
2024-12-04 14:01:51 +0200

    Create React app with Chakra, Vite, Tailwind.

---
+++ Frontend/my-notes/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',  // Здесь пути к вашим файлам
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
